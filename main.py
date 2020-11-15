c3f0a75b8ace61d982f1217c277092de0fa5c54d
Gareth Tribello <gareth.tribello@gmail.com>
2020-11-15 12:39:26 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+def variance_spin( spins ) :
+  # Your code goes here
+  
+  return 
+
+spins1 = [1,-1,1,1,-1,1,1,1,-1,-1]
+print( variance_spin( spins1 ) )
+spins2 = [-1,1,1,1,-1,-1,1,1,1,-1]
+print( variance_spin( spins2 ) )
