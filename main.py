4fbbdeb823e1d4dd16e6f76b2a51188d33829c1c
Gareth Tribello <gareth.tribello@gmail.com>
2020-12-16 12:54:44 +0000

    Added answers

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def variance_spin( spins ) :
   # Your code goes here
-  
-  return 
+  s2, sav = 0, sum(spins) / len(spins)
+  for s in spins : s2 = s2 + s*s 
+  return s2 /len(spins) - sav*sav
 
 spins1 = [1,-1,1,1,-1,1,1,1,-1,-1]
 print( variance_spin( spins1 ) )
